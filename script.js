e8d63b8da63ca40a97005c8cb365f2765bdd3fdd
saibys <85431143+saibys@users.noreply.github.com>
2025-04-27 19:15:49 +0000

    timepop

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 // Show popup after 5 seconds
 setTimeout(() => {
   document.getElementById('popup').style.display = 'block';
-}, 10000);
+}, 15000);
 
 // Close popup when clicking the X button
 
 document.querySelector('.close-btn').addEventListener('click', () => {
-    window.location.href = 'https://api.whatsapp.com/send?phone=918295139188&text=i%20am%20a%20student';
+  window.location.href = 'https://api.whatsapp.com/send?phone=918295139188&text=i%20am%20a%20student';
 });
